b5e28a078f9c83066aa582a588ee66ac1c889014
carnold18 <chrisarnold2010@gmail.com>
2020-01-13 22:06:35 -0800

    adding prework directories and completed FirstConsoleApp

---
+++ FirstConsoleApp/.vs/FirstConsoleApp/FirstConsoleApp/FirstConsoleApp/FirstConsoleApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        public static string title = "C# Essential Training ";
+        public static int whichPart;
+
+        static void Main(string[] args)
+        {
+            whichPart = 2;
+            var message = string.Format("Welcome to {0} Part {1}!", title, whichPart);
+            Console.WriteLine(message);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+        }
+    }
+}
