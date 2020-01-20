735f25168309ec927e452ead4312481ec8b281f7
carnold18 <chrisarnold2010@gmail.com>
2020-01-20 11:33:26 -0800

    set up file structure for unit testing projects

---
+++ C# Essentials Part 2/EssentialTraining/EssentialTrainingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTrainingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTrainingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc36e7cf-4be5-4752-96fa-2202b65ae848")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C# Essentials Part 2/trainingNotesPart2.md
@@ -0,0 +1,17 @@
+# C# TRAINING ESSENTIALS PART 2
+# Flow Control, Arrays, Exception Handling 
+
+## UNIT TESTING & TEST-DRIVEN DEVELOPMENT
+
+ - This is the foundation of Agile development.
+ - In TDD, develop code in phases / features where you develop pieces of code to prove that it runs as designed.
+ - Goal is to have 100% coverage, testing all exposed methods.
+ - 
+
+## ARRAYS & COLLECTION
+
+## FLOW CONTROL
+
+## EXCEPTION HANDLING
+
+## GETTING TO PRDUCTION
